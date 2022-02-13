433bb253af4e678faba0bc937fadd9fb76b732ca
熊谷洸介 <kumagaikousuke1995@gmail.com>
2022-02-13 17:09:36 +0900

    react-router-domの導入

--- react-express/client/src/App.js
+++ react-express/client/src/App.js
@@ -1,28 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
+import Home from "./routes/home";
+import Users from "./routes/users";
+import NoMatch from './routes/nomatch';
+
 const App = () => {
-  const url = "/users";
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    fetch(url, { method: "GET" })
-      .then((res) => res.json())
-      .then((data) => {
-        setUsers(data);
-      })
-      .catch((err) => {
-        console.log(err);
-        console.log("err");
-      });
-  }, []);
-
   return (
     <div className="App">
-      <h1>Users</h1>
-      {users.map((user, index) => (
-        <div key={index}>{user.name}</div>
-      ))}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
     </div>
   );
 };
